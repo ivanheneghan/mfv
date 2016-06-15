0fd6516101d9dc070816e95c27611925ad913603
Ivan Heneghan <ivanheneghan@gmail.com>
2016-06-15 13:23:04 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mfv::Application.config.secret_key_base = '682b27f404512fee5cf4cf4d10c35ce03416453ac70c41d811d9e57eebf2d98489bd20004d12d7f26cb3031ac25a556d112a925bc79213ec651c8a98f9dbe1e0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mfv::Application.config.session_store :cookie_store, key: '_mfv_session'
