f497e35361256a57b29fe47d7bab46f622b7ea6e
Ivan Heneghan <ivanheneghan@gmail.com>
2016-08-01 22:00:05 +0000

    Post-mentor call

--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -1,9 +1,17 @@
 function outputUpdate(val) {
-
   $('#output_future_age').val(document.querySelector('#overview_future_age').value);
   
-
-  // submit the form via ajax/remote
-  //$('#slider_form').submit();
-
 }
+
+
+function bindSliderChangeEvent() {
+  $("#overview_future_age").change(function() {
+    //$("#overview_future_age").disable();
+    $('#slider_form').submit();
+  });
+}
+
+
+$(function() {
+  bindSliderChangeEvent();
+});
--- app/controllers/overviews_controller.rb
+++ app/controllers/overviews_controller.rb
@@ -1,18 +1,26 @@
 class OverviewsController < ApplicationController
 
   before_action :authenticate_user!
+  before_action :build_overview
 
   def show
-    @overview = Overview.new(current_user, (session[:future_age].to_i - current_user.age)).to_hash
   end
 
   def update
     session[:future_age] = overview_params[:future_age]
-    redirect_to overview_path
+
+    respond_to do |format|
+      format.html { redirect_to overview_path }
+      format.js { }
+    end
   end
 
   private
   def overview_params
     params.require(:overview).permit(:future_age)
+  end
+
+  def build_overview
+    @overview = Overview.new(current_user, (session[:future_age].to_i - current_user.age))
   end
 end
